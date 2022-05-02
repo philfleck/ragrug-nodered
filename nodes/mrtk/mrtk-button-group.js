a6e8cc1eff150d8c646bbbd0dbb0e6286fbc8d25
Arkdirfe <g.gotschier@gmail.com>
2022-05-02 13:40:07 +0200

    Added MRTK button group node.

---
+++ nodes/mrtk/mrtk-button-group.js
@@ -0,0 +1,12 @@
+module.exports = function (RED) {
+    function Node(config) {
+        RED.nodes.createNode(this, config);
+
+        this.on('input', function (msg, send, done) {
+            send(msg);
+            done();
+        });
+    }
+
+    RED.nodes.registerType("mrtk-button-group", Node);
+}
